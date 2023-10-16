bea03aa9b9104a6524758e040eae5cc2005f91fa
nadamaged <nadamaged9888@icloud.com>
2023-10-16 19:35:25 +0300

    first commit

---
+++ src/app/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+const AboutMe = () => {
+    return(
+        <>
+        <h1>about me</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit itaque consectetur possimus mollitia explicabo qui!
+            
+        </p>
+        </>
+    )
+}
+
+export default AboutMe;
---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+return (
+    <section>
+        <h2>this is the hero section </h2>
+    </section>
+)
+}
+
+export default HeroSection
